18b38e34c26ef5a3bcd037b0a22cfafea3133b96
marked-gil <markgil_creates@yahoo.com>
2023-01-31 12:45:55 +0000

    Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className="App">
       {/* <ConditionalRenderingFunctional connected={true} /> */}
-      <ConditionalRenderingClass />
+      {/* <ConditionalRenderingClass /> */}
+      <NestingComponents />
     </div>
   );
 }
